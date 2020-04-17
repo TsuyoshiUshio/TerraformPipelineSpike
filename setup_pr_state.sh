7581928be75afb969ac7c42005f4fd7d29d8f387
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-04-17 11:08:22 -0700

    add new script for creating variable group for pr state

---
+++ setup_pr_state.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create a variable group that has variables for managing Pull Request. 
+
+usage() { echo "Usage: setup_pr_state.sh -o <organization> -p <project> -k" 1>&2; exit 1; }
+
+declare organization=""
+declare project=""
+declare stateName="pullrequest.state"
+
+while getopts ":o:p:" arg; do
+    case "${arg}" in
+        o)
+            organization=${OPTARG}
+        ;;
+        p)
+            project=${OPTARG}
+        ;;
+    esac
+done
