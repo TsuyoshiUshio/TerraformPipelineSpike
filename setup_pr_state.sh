9050b9bcd21e5b6e6d5b03d4fc406cc4abc662c5
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-04-17 11:08:22 -0700

    Implement state management for PR flow

---
+++ setup_pr_state.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create a variable group that has variables for managing Pull Request. 
+# e.g. setup_pr_state.sh -o https://dev.azure.com/yourorganization -p your_project_name
+usage() { echo "Usage: setup_pr_state.sh -o <organization> -p <project> -k" 1>&2; exit 1; }
+
+declare organization=""
+declare project=""
+declare stateName="pullrequest.state"
+
+while getopts ":o:p:" arg; do
+    case "${arg}" in
+        o)
+            organization=${OPTARG}
+        ;;
+        p)
+            project=${OPTARG}
+        ;;
+    esac
+done
+
+Id=$(az pipelines variable-group create --authorize true --description "Store a Pipeline State for PullRequest" --org ${organization} --project "${project}" --name ${stateName} --variables "PullRequestNumber=0" | jq -r .id)
+az pipelines variable-group variable create --group-id $Id --org ${organization} --project "${project}" --name "ExpireDate" --value "03/25/2010 14:40"
+az pipelines variable-group variable create --group-id $Id --org ${organization} --project "${project}" --name "Status" --value "Completed"
+
